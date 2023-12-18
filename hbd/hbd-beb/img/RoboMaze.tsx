86ad1c770ecd2cf06c70bb01c8a4bcfce7f90688
shaaarkai <120812911+shaaarkai@users.noreply.github.com>
2023-12-18 20:52:28 +0800

    Add files via upload

---
+++ hbd/hbd-beb/img/RoboMaze.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RoboMaze" tilewidth="16" tileheight="16" tilecount="300" columns="25">
+ <image source="Scaffolding_and_BG_Parts (16 x 16).png" width="400" height="192"/>
+</tileset>
---
+++ hbd/hbd-beb/img/Speech_Emotes (16 x 16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Speech_Emotes (16 x 16)" tilewidth="16" tileheight="16" tilecount="9" columns="9">
+ <image source="../../../Downloads/Mini FX, Items &amp; UI/Mini FX, Items &amp; UI/Mini UI/Speech_Emotes (16 x 16).png" width="144" height="16"/>
+</tileset>
